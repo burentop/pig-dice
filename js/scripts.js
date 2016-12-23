1bc328eb2deb15cdeb1b308e63c6e150ade9ebeb
Brent Perry <burentoperii@gmail.com>
2016-12-23 16:49:01 -0600

    create player objects, and display initial scores.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,23 @@
-function Account(name, deposit) {
+function Player(name) {
   this.name = name;
-  this.balance = deposit;
-}
-
-Account.prototype.alterBalance = function(deposit, withdrawel) {
-  if (deposit) {
-    this.balance += deposit;
-  }
-  if (withdrawel) {
-    this.balance -= withdrawel;
-  }
-  return this.balance;
+  this.score = 0;
 }
 
 $(document).ready(function() {
-  $("form#new-account").submit(function(event) {
+  $("form#new-game").submit(function(event) {
     event.preventDefault();
-    var submittedName = $("#new-name").val();
-    var submittedInitialDeposit = parseInt($("#initial-deposit").val());
+    var playerOneName = $("#player-one").val();
+    var playerTwoName = $("#player-two").val();
+    var playerOne = new Player(playerOneName);
+    var playerTwo = new Player(playerTwoName);
+    $(".player-names").toggle();
 
-    var newAccount = new Account(submittedName, submittedInitialDeposit);
-    $("#balance").text("$" + newAccount.balance.toFixed(2));
-
-    $("#new-name").val("");
-    $("#initial-deposit").val("");
-    $(".alter-submit").show();
-
-    $("form#alter-balance").submit(function(event) {
-      event.preventDefault();
-      var submittedDeposit = parseInt($("#new-deposit").val());
-      var submittedWithdrawel = parseInt($("#new-withdrawel").val());
-
-      newAccount.alterBalance(submittedDeposit, submittedWithdrawel);
-
-      $("#balance").text("");
-      $("#balance").text("$" + newAccount.balance.toFixed(2));
-      $("#new-deposit").val("");
-      $("#new-withdrawel").val("");
-    });
+    $("#player-one-heading").text(playerOne.name);
+    $("#player-two-heading").text(playerTwo.name);
+    $("#player-one-round-score").text(0);
+    $("#player-two-round-score").text(0);
+    $("#player-one-total-score").text(playerOne.score);
+    $("#player-two-total-score").text(playerTwo.score);
+    $(".during-play").show();
   });
-
-
 });
